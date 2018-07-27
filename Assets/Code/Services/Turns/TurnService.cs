c13dc8a200eb3f58b91c94da91900d1f2fa61e15
Nathan Reid <nate@nateandmor.com>
2018-07-27 18:55:04 -0500

    implement TurnService sorting

--- Assets/Code/Services/Turns/TurnService.cs
+++ Assets/Code/Services/Turns/TurnService.cs
@@ -2,29 +2,14 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 
-public class TurnService
-{
+public class TurnService {
   private readonly Contexts _contexts;
 
-  class EntitySpeed
-  {
-    public readonly int entityId;
-    public int speed;
+  private readonly List<EntitySpeed> _entities = new List<EntitySpeed>();
 
-    public EntitySpeed(int entityId, int speed)
-    {
-      this.entityId = entityId;
-      this.speed = speed;
-    }
-  }
-
-  private List<EntitySpeed> _entities = new List<EntitySpeed>();
-
-  public TurnService(Contexts contexts)
-  {
+  public TurnService(Contexts contexts) {
     _contexts = contexts;
-    _entities.AddRange(new[]
-    {
+    _entities.AddRange(new[] {
       new EntitySpeed(-1, 5),
       new EntitySpeed(-1, 15),
       new EntitySpeed(-1, 54),
@@ -34,57 +19,54 @@ public class TurnService
     _entities.Sort(new SpeedSortComparer());
   }
 
-  public void RemoveEntity(int entityId)
-  {
+  public void RemoveEntity(int entityId) {
     _entities.RemoveAt(_entities.FindIndex(entity => entity.entityId == entityId));
   }
 
-  public void AddEntity(int entityId, int speed)
-  {
+  public void AddEntity(int entityId, int speed) {
     var entitySpeed = new EntitySpeed(entityId, speed);
-    var index = _entities.BinarySearch(entitySpeed, new SpeedIndexComparer(entitySpeed));
+    var index = _entities.BinarySearch(entitySpeed, new SpeedIndexComparer());
     _entities.Insert(index, entitySpeed);
-    //    Debug.Log("index");
-//    _entities.Insert(entityId);
   }
 
-  class SpeedSortComparer : IComparer<EntitySpeed>
-  {
+  
+  public class EntitySpeed {
+    public readonly int entityId;
+    public int speed;
+
+    public EntitySpeed(int entityId, int speed) {
+      this.entityId = entityId;
+      this.speed = speed;
+    }
+  }
+  
+  public class SpeedSortComparer : IComparer<EntitySpeed> {
     private readonly EntitySpeed _entitySpeed;
 
-    public SpeedSortComparer(EntitySpeed entitySpeed)
-    {
+    public SpeedSortComparer(EntitySpeed entitySpeed) {
       _entitySpeed = entitySpeed;
     }
 
-    public SpeedSortComparer()
-    {
-    }
+    public SpeedSortComparer() { }
 
-    public int Compare(EntitySpeed x, EntitySpeed y)
-    {
-      if (x == null && y == null)
-      {
+    public int Compare(EntitySpeed x, EntitySpeed y) {
+      if (x == null && y == null) {
         return 0;
       }
-      
-      if (x == null)
-      {
+
+      if (x == null) {
         return 1;
       }
 
-      if (y == null)
-      {
-        return -1;
-      }
-      
-      if (x.speed > y.speed)
-      {
+      if (y == null) {
         return -1;
       }
 
-      if (x.speed < y.speed)
-      {
+      if (x.speed > y.speed) {
+        return -1;
+      }
+
+      if (x.speed < y.speed) {
         return 1;
       }
 
@@ -92,29 +74,24 @@ public class TurnService
     }
   }
 
-
-  class SpeedIndexComparer : IComparer<EntitySpeed>
-  {
-    private readonly EntitySpeed _entitySpeed;
-
-    public SpeedIndexComparer(EntitySpeed entitySpeed)
-    {
-      _entitySpeed = entitySpeed;
-    }
-
-    public int Compare(EntitySpeed x, EntitySpeed y)
-    {
-      if (x.speed > y.speed)
-      {
-        return -1;
-      }
-
-      if (x.speed < y.speed)
-      {
+  public class SpeedIndexComparer : IComparer<EntitySpeed> {
+    public int Compare(EntitySpeed x, EntitySpeed y) {
+      if (x == null) {
+        if (y == null) {
+          return 0;
+        }
+        
         return 1;
       }
 
-      return 0;
+      if (y == null) {
+        return -1;
+      }
+      if (x.speed >= y.speed) {
+        return -1;
+      }
+
+      return 1;
     }
   }
 }
